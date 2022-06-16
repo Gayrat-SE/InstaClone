ae4854dc83144e046b8552943a38927177404967
Gayrat <gayratse@gmail.com>
2022-06-16 18:18:36 +0500

    post form added

--- post/urls.py
+++ post/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('newpost/', views.newPost, name='newpost'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,9 +1,8 @@
-from calendar import c
-from multiprocessing import context
-from pkgutil import ImpImporter
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from .models import Post, Tag, Follow, Stream
 from django.contrib.auth.decorators import login_required
+from .forms import  NewPostform
 # Create your views here.
 
 
@@ -15,5 +14,34 @@ def index(request):
         group_ids.append(post.post.id)
     post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
     context = {'post_items': post_items}
-
+    
     return render(request, 'index.html',context)
+
+
+
+
+def newPost(request):
+    user = request.user
+    tags_obj = []
+    
+    if request.method == "POST":
+        form = NewPostform(request.POST, request.FILES)
+        if form.is_valid():
+            picture = form.cleaned_data.get('picture')
+            caption = form.cleaned_data.get('caption')
+            tag_form = form.cleaned_data.get('tag')
+            tag_list = list(tag_form.split(','))
+
+            for tag in tag_list:
+                t, created = Tag.objects.get_or_create(title=tag)
+                tags_obj.append(t)
+            p, created = Post.objects.get_or_create(picture=picture, caption=caption, user=user)
+            p.tag.set(tags_obj)
+            p.save()
+            return redirect('index')
+    else:
+        form = NewPostform()
+    context = {
+        'form': form
+    }
+    return render(request, 'newpost.html', context)            
